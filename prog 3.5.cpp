0b613c9378899a21092010c4d102a9af1784f38f
Omdev-Paul <101262334+Omdev-Paul@users.noreply.github.com>
2022-10-07 09:15:21 +0530

    Add files via upload

---
+++ prog 3.5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+(
+    int ivar=25;
+    float fvar=35.87;
+    cout<<"intvar= "<<ivar<<endl;
+    cout<<"flaotvar= "<<fvar<<endl;
+    cout<<"float(intvar)= "<<float(ivar)r<<endl;
+    cout<<"int(flaotvar)= "<<int(fvar)<<endl;
+    return 1;
+)
